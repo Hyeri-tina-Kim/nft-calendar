bcfbbe2a0cb099eec85409f3c1d06379b04ec508
DESKTOP-HSIM28N\82109 <tina724dev@gmail.com>
2022-04-02 22:30:15 +0900

    fix: dynamic import

--- pages/TuiCalendarWrapper.tsx
+++ pages/TuiCalendarWrapper.tsx
@@ -1,4 +1,8 @@
 import React from "react";
-import Calendar from "@toast-ui/react-calendar";
+import dynamic from "next/dynamic";
+
+const Calendar = dynamic(() => import("@toast-ui/react-calendar"), {
+  ssr: false,
+});
 
 export default (props: any) => <Calendar {...props} ref={props.forwardedRef} />;
