f083c090f78eee06cc499023b13757f772dc8018
DESKTOP-HSIM28N\82109 <tina724dev@gmail.com>
2022-04-14 20:15:16 +0900

    build

--- pages/TuiCalendarWrapper.tsx
+++ pages/TuiCalendarWrapper.tsx
@@ -1,18 +1,22 @@
 import React, { HTMLAttributes, FC, LegacyRef } from "react";
-import Calendar, { EventMaps } from "@toast-ui/react-calendar";
+// import Calendar, { EventMaps } from "@toast-ui/react-calendar";
+import dynamic from "next/dynamic";
+const Calendar = dynamic(() => import("@toast-ui/react-calendar"), {
+  ssr: false,
+});
 import { IOptions, ISchedule } from "tui-calendar";
 
-export type TUICalendarProps = IOptions &
-  EventMaps & {
-    height: string;
-    view?: string;
-    schedules?: ISchedule[];
-  } & HTMLAttributes<HTMLElement>;
+// export type TUICalendarProps = IOptions &
+//   EventMaps & {
+//     height: string;
+//     view?: string;
+//     schedules?: ISchedule[];
+//   } & HTMLAttributes<HTMLElement>;
 
-type TUICalendarPropsWithRef = {
-  forwardedRef: LegacyRef<Calendar>;
-} & TUICalendarProps;
+// type TUICalendarPropsWithRef = {
+//   forwardedRef: LegacyRef<Calendar>;
+// } & TUICalendarProps;
 
-export default (props: TUICalendarPropsWithRef) => {
+export default (props: any) => {
   return <Calendar {...props} ref={props.forwardedRef} />;
 };
